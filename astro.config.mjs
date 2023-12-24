a8d93fa3ac9c2d59c4b23598cae2a0a3cabbe0d8
Mridul Rathore <44365725+mridulmr@users.noreply.github.com>
2023-12-24 19:56:13 +0530

    updated the robot.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,30 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import compress from "astro-compress";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()]
+  site: "https://bitwit-techno-site.web.app",
+  integrations: [
+    tailwind(),
+    compress(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-ES",
+          fr: "fr-CA",
+        },
+      },
+      lastmod: new Date(),
+    }),
+  ],
 });
