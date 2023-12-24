e97c7a8fff94ca187b4247aa4000989eada2b433
Mridul Rathore <44365725+mridulmr@users.noreply.github.com>
2023-12-24 19:45:00 +0530

    updated the library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr", "pt-br", "es"],
-  },
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()]
 });
