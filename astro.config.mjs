c904ded5997b79fb8352407442a856e4fd505bdf
Mridul Rathore <44365725+mridulmr@users.noreply.github.com>
2023-12-24 23:29:05 +0530

    updated the babel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,36 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
-import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+import babelScripts from "astro-babel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bitwit-techno-site.web.app",
-  integrations: [
-    tailwind(),
-    compress(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    sitemap({
-      i18n: {
-        defaultLocale: "en",
-        locales: {
-          en: "en-US",
-          es: "es-ES",
-          fr: "fr-CA",
-        },
-      },
-      lastmod: new Date(),
-    }),
-  ],
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap({
+    i18n: {
+      defaultLocale: "en",
+      locales: {
+        en: "en-US",
+        es: "es-ES",
+        fr: "fr-CA"
+      }
+    },
+    lastmod: new Date()
+  }), babelScripts({
+    presets: [
+      ['minify', {
+      builtIns: false,
+      evaluate: false,
+      mangle: false,
+      }], 
+      ["@babel/preset-env", { 
+        "modules": false 
+      }]
+    ]
+  })]
 });
