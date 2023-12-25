7205a9b3694a3bc46c5562d323576666476bc649
Mridul Rathore <44365725+mridulmr@users.noreply.github.com>
2023-12-25 11:03:50 +0530

    updated the robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,50 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import babelScripts from "astro-babel";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
+import babelScripts from 'astro-babel'
+
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bitwit-techno-site.web.app",
-  integrations: [tailwind(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), sitemap({
-    i18n: {
-      defaultLocale: "en",
-      locales: {
-        en: "en-US",
-        es: "es-ES",
-        fr: "fr-CA"
+  site: 'https://bitwit-techno-site.web.app',
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push']
       }
-    },
-    lastmod: new Date()
-  }), babelScripts({
-    presets: [
-      ['minify', {
-      builtIns: false,
-      evaluate: false,
-      mangle: false,
-      }], 
-      ["@babel/preset-env", { 
-        "modules": false 
-      }]
-    ]
-  })]
-});
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          es: 'es-ES',
+          fr: 'fr-CA'
+        }
+      },
+      lastmod: new Date()
+    }),
+    babelScripts({
+      presets: [
+        [
+          'minify',
+          {
+            builtIns: false,
+            evaluate: false,
+            mangle: false
+          }
+        ],
+        [
+          '@babel/preset-env',
+          {
+            modules: false
+          }
+        ]
+      ]
+    }),
+    robotsTxt()
+  ]
+})
