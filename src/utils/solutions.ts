6842913d58fb8843e02d318895dd82a9f5da144b
Mridul Rathore <mridulmr@outlook.com>
2024-01-01 21:53:52 +0530

    updated the content

---
+++ src/utils/solutions.ts
@@ -0,0 +1,38 @@
+export const onDemandSolution = [
+  {
+    icon: 'onDemandSolutionIcon[0]',
+    title: 'Lifestyle Apps',
+    description:
+      'Digital Solutions for Enhanced Living: Explore our Lifestyle Apps for a Seamless and Elevated Everyday Experience.'
+  },
+  {
+    icon: 'onDemandSolutionIcon[1]',
+    title: 'Social Media Apps',
+    description:
+      'Enhance your online presence with cutting-edge social media apps, optimizing digital interaction and engagement for your business. Discover how our IT solutions can elevate your social media strategy.'
+  },
+  {
+    icon: 'onDemandSolutionIcon[2]',
+    title: 'Utility Apps',
+    description:
+      'Explore our suite of indispensable utility apps designed to boost productivity and simplify daily tasks. Discover how our cutting-edge software solutions can streamline your operations and optimize efficiency'
+  },
+  {
+    icon: 'onDemandSolutionIcon[3]',
+    title: 'Productivity Apps',
+    description:
+      'Optimize Efficiency with Cutting-Edge Productivity Applications: Explore our suite of advanced productivity software solutions designed to streamline workflows and enhance organizational efficiency.'
+  },
+  {
+    icon: 'onDemandSolutionIcon[4]',
+    title: 'Entertainment Apps',
+    description:
+      'Elevate User Experience with Innovative IT Solutions. Discover our suite of entertainment apps designed to captivate and engage users, redefining digital entertainment. Harness the power of technology to create immersive, user-centric experiences.'
+  },
+  {
+    icon: 'onDemandSolutionIcon[5]',
+    title: 'Media Apps',
+    description:
+      'Elevating User Experiences and Engagement. Explore our cutting-edge media apps to drive immersive content delivery and audience interaction.'
+  }
+]
